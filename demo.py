a472fff3ced77fa526f8d99bed2b4a55e26a3995
GO-xiaoqin <55121236@qq.com>
2020-04-06 14:49:45 +0800

    python

---
+++ demo.py
@@ -0,0 +1,10 @@
+'''
+@Author: your name
+@Date: 2020-04-06 14:46:51
+@LastEditTime: 2020-04-06 14:48:10
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \git_project\python_project\demo.py
+'''
+
+print('hello!!')
